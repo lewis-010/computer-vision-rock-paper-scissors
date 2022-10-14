025a4cf1f9957b92fee6ba6a2c8b918e99c18120
Lewis Cox <lcox101101@gmail.com>
2022-10-14 16:23:23 +0100

    create function for computer choice

--- manual_rps.py
+++ manual_rps.py
@@ -1,6 +1,14 @@
 import random
 
+def get_computer_choice():
+    with open("labels.txt") as file:
+        allText = file.read()
+        words = list(map(str, allText.split()))
+        computer_choice = random.choice(words)
+    return computer_choice
+
+
 def get_user_choice():
-    user = input("Enter your choice: ")
-    return user
+    user_choice = input("Enter your choice: ")
+    return user_choice
 
