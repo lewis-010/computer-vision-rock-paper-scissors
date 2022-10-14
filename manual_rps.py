2434d4f33eeb0fb48a08a43c17207fd32206e991
Lewis Cox <lcox101101@gmail.com>
2022-10-14 17:02:30 +0100

    computer choice function choose from list

--- manual_rps.py
+++ manual_rps.py
@@ -1,16 +1,14 @@
 import random
 
 def get_computer_choice():
-    with open("labels.txt") as file:
-        allText = file.read()
-        words = list(map(str, allText.split()))
-        computer_choice = random.choice(words)
-        print(computer_choice)
+    options = ['Rock', 'Paper', 'Scissors']
+    computer_choice = random.choice(options)
+    print(computer_choice)
     return computer_choice
 
 
 def get_user_choice():
-    user_choice = input("Enter your choice: ")
+    user_choice = input('Enter your choice: ')
     return user_choice
 
 get_computer_choice()
