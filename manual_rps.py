c4d1f559278d57eac1d97e87c592d64008c9c7a2
Lewis Cox <lcox101101@gmail.com>
2022-10-15 09:43:37 +0100

    create get_winner function

--- manual_rps.py
+++ manual_rps.py
@@ -1,9 +1,9 @@
 import random
 
 def get_computer_choice():
-    options = ['Rock', 'Paper', 'Scissors']
+    options = ['rock', 'paper', 'scissors']
     computer_choice = random.choice(options)
-    print(computer_choice)
+    # print(computer_choice)
     return computer_choice
 
 
@@ -11,5 +11,15 @@ def get_user_choice():
     user_choice = input('Enter your choice: ')
     return user_choice
 
-get_computer_choice()
-get_user_choice()
+def get_winner(computer_choice, user_choice):
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    if (computer_choice=="rock" and user_choice=="scissors") or (computer_choice=="paper" and user_choice=="rock") or (computer_choice=="scissors" and user_choice=="paper"):
+        print("Computer wins!")
+    elif (computer_choice=="rock" and user_choice=="paper") or (computer_choice=="paper" and user_choice=="scissors") or (computer_choice=="scissors" and user_choice=="rock"):
+        print("You win!")
+    else:
+        print("Draw!")
+
+get_winner(computer_choice=get_computer_choice, user_choice=get_user_choice)
+
