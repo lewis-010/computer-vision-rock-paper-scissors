18764b51e1af9112ccd990ed74d3d49436467a51
Lewis Cox <lcox101101@gmail.com>
2022-10-15 10:17:18 +0100

    play() function calls all three previous functions

--- manual_rps.py
+++ manual_rps.py
@@ -12,8 +12,6 @@ def get_user_choice():
     return user_choice
 
 def get_winner(computer_choice, user_choice):
-    computer_choice = get_computer_choice()
-    user_choice = get_user_choice()
     if (computer_choice=="rock" and user_choice=="scissors") or (computer_choice=="paper" and user_choice=="rock") or (computer_choice=="scissors" and user_choice=="paper"):
         print("Computer wins!")
     elif (computer_choice=="rock" and user_choice=="paper") or (computer_choice=="paper" and user_choice=="scissors") or (computer_choice=="scissors" and user_choice=="rock"):
@@ -22,8 +20,6 @@ def get_winner(computer_choice, user_choice):
         print("Draw!")
 
 def play():
-    get_computer_choice()
-    get_user_choice()
-    get_winner(computer_choice=get_computer_choice, user_choice=get_user_choice)
+    get_winner(computer_choice=get_computer_choice(), user_choice=get_user_choice())
 
 play()
