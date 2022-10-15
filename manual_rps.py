9e5d2c916ce6f5571e1fc2cfff23497c7e670da3
Lewis Cox <lcox101101@gmail.com>
2022-10-15 10:34:44 +0100

    add comments

--- manual_rps.py
+++ manual_rps.py
@@ -1,16 +1,18 @@
 import random
 
+# gets computer's choice of rock, paper or scissors
 def get_computer_choice():
     options = ['rock', 'paper', 'scissors']
     computer_choice = random.choice(options)
-    print(computer_choice)
+    # print(computer_choice)
     return computer_choice
 
-
+# gets user's choice of rock, paper or scissors
 def get_user_choice():
     user_choice = input('Enter your choice: ')
     return user_choice
 
+# determines winner based on value of computer_choice and user_choice
 def get_winner(computer_choice, user_choice):
     if (computer_choice=="rock" and user_choice=="scissors") or (computer_choice=="paper" and user_choice=="rock") or (computer_choice=="scissors" and user_choice=="paper"):
         print("Computer wins!")
@@ -19,6 +21,7 @@ def get_winner(computer_choice, user_choice):
     else:
         print("Draw!")
 
+# calls all three previous functions to play the game
 def play():
     get_winner(computer_choice=get_computer_choice(), user_choice=get_user_choice())
 
