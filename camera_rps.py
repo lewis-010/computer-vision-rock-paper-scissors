4efc1423caa842c8eb3c497162976821d318eda7
Lewis Cox <lcox101101@gmail.com>
2022-10-21 11:08:25 +0100

    add docstrings for all functions

--- camera_rps.py
+++ camera_rps.py
@@ -29,25 +29,30 @@ class Rps:
         Randomly selects the computer's choice for each game from computer_options list.
 
         Returns:
-            rock, paper or scissors.
+            computer_choice: rock, paper or scissors.
         '''
         computer_options = ['rock', 'paper', 'scissors']
         computer_choice = random.choice(computer_options)
         return computer_choice
 
     def get_user_choice(self):
+        '''
+        Uses the computer vision model (keras_model.h5) to determine the user's choice. 
+        Runs inside the while loop for 1 second and outputs text to camera feed describing user's choice (cv2.putText)
+
+        Returns:
+            user_choice: rock, paper, scissors or nothing.
+        '''
         t_end = time.time()+1
         while t_end > time.time(): 
             ret, frame = cap.read()
             font = cv2.FONT_HERSHEY_SIMPLEX
             resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
             image_np = np.array(resized_frame)
-            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+            normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # normalize the image
             data[0] = normalized_image
             prediction = model.predict(data)
             user_choice = np.argmax(prediction)
-
-            # add user guess as text to camera feed in
             cv2.putText(frame, f"User Choice {self.options[user_choice]} ", (50, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
             cv2.imshow('frame', frame)
             # Press q to close the window
@@ -56,6 +61,13 @@ class Rps:
         return self.options[user_choice].lower()
 
     def get_winner(self):
+        '''
+        Runs the above two functions to get user/computer choice and determines winner based on if functions.
+
+        Output:
+            print statement describing the winner.
+            adds 1 to the relecant class attribute (user_wins or computer_wins)
+        '''
         computer_choice = self.get_computer_choice()
         user_choice = self.get_user_choice()
         print("Computer choice:", computer_choice)
@@ -71,18 +83,32 @@ class Rps:
             print("Draw!")           
 
     def countdown(self):
-            print("The game will start soon, make your choice now!")
-            time.sleep(3)
-            print("ROCK")
-            time.sleep(1)
-            print("PAPER")
-            time.sleep(1)
-            print("SCISSORS")
-            time.sleep(1)
-            print("SHOOT")
-            time.sleep(1)
+        '''
+        Runs a countdown in the form of a classic rock, paper, scissors game. 
+        Allows time for user to make a choice.
+
+        Output:
+            print statements: ROCK, PAPER, SCISSORS, SHOOT
+        '''
+        print("The game will start soon, make your choice now!")
+        time.sleep(3)
+        print("ROCK")
+        time.sleep(1)
+        print("PAPER")
+        time.sleep(1)
+        print("SCISSORS")
+        time.sleep(1)
+        print("SHOOT")
+        time.sleep(1)
 
 def play_game():
+    '''
+    Instance of the class Rps created (play). Runs the above functions to play the game (limit of 3 wins set before game ends).
+
+    Output:
+        print statememt for winner of each game.
+        print statememt for winner of the overall game once 3 have been won by user or computer.
+    '''
     print("Let's play rock, paper, scissors. First to three wins!")
     time.sleep(2)
     play = Rps()
